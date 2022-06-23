239ea69740b6de1b1ce4580d076f43bf4d2c5d25
Ry <ryanst755@gmail.com>
2022-06-23 00:35:54 -0700

    Add allocate demo

---
+++ allocate/allocate.asm
@@ -0,0 +1,42 @@
+; memory allocation demo
+
+    ; allocate first 4 byte block
+    mov r0, 4
+    call allocate_memory
+
+    brk
+
+    mov r1, r0
+
+    ; allocate second 4 byte block
+    mov r0, 4
+    call allocate_memory
+
+    brk
+
+    ; free the first block
+    mov r0, r1
+    call free_memory
+
+    ; allocate 16 byte block
+    mov r0, 16
+    call allocate_memory
+
+    brk
+
+    ; allocate third 4 byte block
+    mov r0, 4
+    call allocate_memory
+
+    brk
+
+    ; allocate fourth 4 byte block
+    mov r0, 4
+    call allocate_memory
+
+    brk
+
+    rjmp 0
+
+    #include "../../fox32rom/fox32rom.def"
+    #include "../../fox32os/fox32os.def"
