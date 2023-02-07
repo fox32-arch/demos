f3882e214afa83d1e3cf2737d0803d88b485795a
Ry <ryanst755@gmail.com>
2023-02-06 19:53:11 -0800

    gcc: Update to use new C bindings

--- gcc/hello.c
+++ gcc/hello.c
@@ -1,36 +1,7 @@
-typedef unsigned int uint32_t;
-
-#define WIDTH 640
-#define HEIGHT 480
-
-static volatile uint32_t *const fb = (uint32_t *)0x2000000;
-
-static void draw_px(int x, int y, uint32_t color) {
-	fb[x + WIDTH * y] = color;
-}
-
-static void draw_hline(int x0, int x1, int y, uint32_t color) {
-	for (int x = x0; x <= x1; x++)
-		draw_px(x, y, color);
-}
-
-static void draw_vline(int x, int y0, int y1, uint32_t color) {
-	for (int y = y0; y <= y1; y++)
-		draw_px(x, y, color);
-}
-
-#define COLOR_WHITE 0xffffffff
+#include "../../tools/gcc/bindings.c"
 
 int main(void) {
-	draw_px(20, 20, COLOR_WHITE); // dot
-
-	draw_vline(100, 100, 300, COLOR_WHITE); // H
-	draw_vline(200, 100, 300, COLOR_WHITE);
-	draw_hline(100, 200, 200, COLOR_WHITE);
-
-	draw_hline(250, 350, 100, COLOR_WHITE); // I
-	draw_vline(300, 100, 300, COLOR_WHITE);
-	draw_hline(250, 350, 300, COLOR_WHITE);
-
-	return 0;
+    draw_str_to_background("hello world!!!", 16, 16, 0xFFFFFFFF, 0xFF000000);
+    draw_decimal_to_background(random_range(1, 7), 16, 32, 0xFFFFFFFF, 0xFF000000);
+    return 0;
 }
