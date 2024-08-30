c9e1922ce9bf4eedf6905db586083f31428cd281
Ry <ryanst755@gmail.com>
2024-08-29 18:50:04 -0700

    tcc: Optimizations

--- boot/tcc.asm
+++ boot/tcc.asm
@@ -4,26 +4,26 @@
     opton
     org 0x00000800
 
+entry:
     mov r11, 479 ; Y counter
 y_loop:
-    mov r10, 639 ; X counter
+    mov r31, 639 ; X counter
 x_loop:
-    mov r0, r10
+    mov r0, r31
     mov r1, r11
     add r1, 16
-    mov r2, r10
+    mov r2, r31
     xor r2, r11
     rem r2, 3
-    mul r2, 128
-    or r2, 0xFF000000
+    sla r2, 7
     call [0xF004201C] ; draw_pixel_to_background
-    dec r10
-    ifnz rjmp x_loop
+    rloop.8 x_loop
     dec r11
-    ifnz rjmp y_loop
+    ifnz rjmp.8 y_loop
 
-    rjmp 0
+    rjmp.8 entry
 
     ; bootable magic bytes
+    nop ; work around an assembler bug with org.pad coming after a non-32-bit instruction
     org.pad 0x000009FC
     data.32 0x523C334C
